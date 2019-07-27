a2686fce250493597ccf198aa29b13f6b67417c0
astrongstorm <caomichael19@gmail.com>
2019-07-27 21:48:30 +0800

    Solve out of memory problem

--- visda_classification/dev.py
+++ visda_classification/dev.py
@@ -50,6 +50,7 @@ def get_weight(source_feature, target_feature,
     :param validation_feature: shape [N_v, d], features from validation set
     :return:
     """
+    print("Start calculating weight")
     N_s, d = source_feature.shape
     N_t, _d = target_feature.shape
     if float(N_s) / N_t > 2:
@@ -74,7 +75,7 @@ def get_weight(source_feature, target_feature,
     domain_classifiers = []
 
     for decay in decays:
-        domain_classifier = MLPClassifier(hidden_layer_sizes=(d, d, 2), activation='relu', alpha=decay)
+        domain_classifier = MLPClassifier(hidden_layer_sizes=(d, d, 2), activation='relu', alpha=decay, max_iter=5)
         domain_classifier.fit(feature_for_train, label_for_train)
         output = domain_classifier.predict(feature_for_test)
         acc = np.mean((label_for_test == output).astype(np.float32))
@@ -127,7 +128,7 @@ def predict_loss(cls, y_pre):
     pre_cls_torch = y_pre.double()
     target = torch.from_numpy(cls_torch).cuda()
     entropy = nn.CrossEntropyLoss()
-    return entropy(pre_cls_torch, target)
+    return entropy(pre_cls_torch, target).detach().cpu()
 
 def get_label_list(args, target_list, feature_network_path, predict_network_path, num_layer, resize_size, crop_size, batch_size, use_gpu):
     """
@@ -178,7 +179,7 @@ def get_label_list(args, target_list, feature_network_path, predict_network_path
     return label_list
 
 
-def cross_validation_loss(args, feature_network_path, predict_network_path, num_layer, src_cls_list, target_path, val_cls_list, class_num,
+def cross_validation_loss(args, feature_network_path, predict_network_path, num_layer, src_list, target_path, val_list, class_num,
                           resize_size, crop_size, batch_size, use_gpu):
     """
     Main function for computing the CV loss
@@ -194,17 +195,8 @@ def cross_validation_loss(args, feature_network_path, predict_network_path, num_
     :return:
     """
     target_list_no_label = open(target_path).readlines()
-    tar_cls_list = []
     cross_val_loss = 0
-
-    # add pesudolabel for target data
-    target_list = get_label_list(args, target_list_no_label, feature_network_path, predict_network_path, num_layer,
-                                 resize_size, crop_size, batch_size, use_gpu)
-
-    # seperate the class
-    for i in range(class_num):
-        tar_cls_list.append([j for j in target_list if int(j.split(" ")[1].replace("\n", "")) == i])
-    prep_dict = prep.image_train(resize_size=resize_size, crop_size=crop_size)
+    save_path = args.save
     # load network
     option = 'resnet' + args.resnet
     G = ResBase(option)
@@ -216,114 +208,182 @@ def cross_validation_loss(args, feature_network_path, predict_network_path, num_
         F1.cuda()
     G.eval()
     F1.eval()
-    # load different class's image
+    print("Loaded network")
+    # add pesudolabel for target data
+    print("Sperating target data")
+    tar_list = []
+    dsets_tar = ImageList(target_list_no_label,
+                          transform=prep.image_train(resize_size=resize_size, crop_size=crop_size))
+    dset_loaders_tar = util_data.DataLoader(dsets_tar, batch_size=batch_size, shuffle=False, num_workers=4)
+    len_train_target = len(dset_loaders_tar)
+    iter_target = iter(dset_loaders_tar)
+    count = 0
+    for i in range(len_train_target):
+        input_tar, label_tar = iter_target.next()
+        if use_gpu:
+            input_tar, label_tar = Variable(input_tar).cuda(), Variable(label_tar).cuda()
+        else:
+            input_tar, label_tar = Variable(input_tar), Variable(label_tar)
+        tar_feature = G(input_tar)
+        predict_score = F1(tar_feature)
+        _, pre_lab = torch.max(predict_score, 1)
+        predict_label = pre_lab.detach()
+        for num in range(len(predict_label.cpu())):
+            if target_list_no_label[count][-3] == ' ':
+                ind = -2
+            else:
+                ind = -3
+            tar_list.append(target_list_no_label[count][:ind])
+            tar_list[count] = tar_list[count] + str(predict_label[num].cpu().numpy()) + "\n"
+            count += 1
+    val_list = seperate_data.dimension_rd(val_list)
+    print("Seperated")
+    # load the dataloader for whole data
+    prep_dict = prep.image_train(resize_size=resize_size, crop_size=crop_size)
+    dsets_src = ImageList(src_list, transform=prep_dict)
+    dset_loaders_src = util_data.DataLoader(dsets_src, batch_size=batch_size, shuffle=True, num_workers=4)
+    dsets_val = ImageList(val_list, transform=prep_dict)
+    dset_loaders_val = util_data.DataLoader(dsets_val, batch_size=batch_size, shuffle=True, num_workers=4)
+    dsets_tar = ImageList(tar_list, transform=prep_dict)
+    dset_loaders_tar = util_data.DataLoader(dsets_tar, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    # iterate through different classes
     for cls in range(class_num):
-        print(cls)
         # prepare source feature
-        dsets_src = ImageList(src_cls_list[cls], transform=prep_dict)
-        dset_loaders_src = util_data.DataLoader(dsets_src, batch_size=batch_size, shuffle=True, num_workers=4)
-
+        count_src = 0
+        src_feature_de = np.array([])
         iter_src = iter(dset_loaders_src)
-        src_input = iter_src.next()[0]
-        if use_gpu:
-            src_input = Variable(src_input).cuda()
-        else:
-            src_input = Variable(src_input)
+        while src_feature_de.size == 0:
+            src_input, src_labels = iter_src.next()
+            for i in range(len(src_labels)):
+                if src_labels[i].item() == cls:
+                    a, b, c = src_input[i].shape
+                    if use_gpu:
+                        src_pre_input = Variable(src_input[i]).cuda()
+                    else:
+                        src_pre_input = Variable(src_input[i])
+                    src_input_final = src_pre_input.reshape(1, a, b, c)
+                    if src_feature_de.size == 0:
+                        src_feature = G(src_input_final)
+                        src_feature_de = src_feature.detach().detach().cpu().numpy()
+                    else:
+                        src_feature_new = G(src_input_final)
+                        src_feature_new_de = src_feature_new.detach().cpu().numpy()
+                        src_feature_de = np.append(src_feature_de, src_feature_new_de, axis=0)
+            count_src = count_src + 1
+        for _ in range(len(dset_loaders_src) - count_src):
+            src_input, src_labels = iter_src.next()
+            for i in range(len(src_labels)):
+                print(src_input.shape)
+                print(src_labels.shape)
+                if src_labels[i].item() == cls:
+                    print(src_input[i].shape)
+                    a, b, c = src_input[i].shape
+                    if use_gpu:
+                        src_pre_input = Variable(src_input[i]).cuda()
+                    else:
+                        src_pre_input = Variable(src_input[i])
+                    src_input_final = src_pre_input.reshape(1, a, b, c)
+                    src_feature_new = G(src_input_final)
+                    src_feature_new_de = src_feature_new.detach().cpu().numpy()
+                    src_feature_de = np.append(src_feature_de, src_feature_new_de, axis=0)
+        print("Created feature: {}".format(src_feature_de.shape))
+        print("Pass Source for Class{}".format(cls + 1))
 
-        src_feature = G(src_input)
-        src_feature_de = src_feature.detach().cpu().numpy()
-        for count_src in range(len(dset_loaders_src) - 1):
-            src_input = iter_src.next()[0]
-            if use_gpu:
-                src_input = Variable(src_input).cuda()
-            else:
-                src_input = Variable(src_input)
-
-            src_feature_new = G(src_input)
-            src_feature_new_de = src_feature_new.detach().cpu().numpy()
-            src_feature_de = np.append(src_feature_de, src_feature_new_de, axis=0)
-
-        # prepare target feature
-        dsets_tar = ImageList(tar_cls_list[cls], transform=prep_dict)
-        dset_loaders_tar = util_data.DataLoader(dsets_tar, batch_size=batch_size, shuffle=True, num_workers=4)
-
+        # prepare target fature
+        count_tar = 0
+        tar_feature_de = np.array([])
         iter_tar = iter(dset_loaders_tar)
-        tar_input = iter_tar.next()[0]
-        if use_gpu:
-            tar_input = Variable(tar_input).cuda()
-        else:
-            tar_input = Variable(tar_input)
+        while tar_feature_de.size == 0:
+            tar_input, tar_labels = iter_tar.next()
+            for i in range(len(tar_labels)):
+                if tar_labels[i].item() == cls:
+                    a, b, c = tar_input[i].shape
+                    if use_gpu:
+                        tar_pre_input = Variable(tar_input[i]).cuda()
+                    else:
+                        tar_pre_input = Variable(tar_input[i])
+                    tar_input_final = tar_pre_input.reshape(1, a, b, c)
+                    tar_feature = G(tar_input_final)
+                    if tar_feature_de.size == 0:
+                        tar_feature_de = tar_feature.detach().cpu().numpy()
+                    else:
+                        tar_feature_new_de = tar_feature.detach().cpu().numpy()
+                        tar_feature_de = np.append(tar_feature_de, tar_feature_new_de, axis=0)
+            count_tar = count_tar + 1
+        for _ in range(len(dset_loaders_tar) - count_tar):
+            tar_input, tar_labels = iter_tar.next()
+            for i in range(len(tar_labels)):
+                if tar_labels[i].item() == cls:
+                    a, b, c = tar_input[i].shape
+                    if use_gpu:
+                        tar_pre_input = Variable(tar_input[i]).cuda()
+                    else:
+                        tar_pre_input = Variable(tar_input[i])
+                    tar_input_final = tar_pre_input.reshape(1, a, b, c)
+                    tar_feature_new = G(tar_input_final)
+                    tar_feature_new_de = tar_feature_new.detach().cpu().numpy()
+                    tar_feature_de = np.append(tar_feature_de, tar_feature_new_de, axis=0)
+        print("Pass Target for Class: {}".format(cls + 1))
+        print("Created feature: {}".format(tar_feature_de.shape))
+        # prepare validation feature and errors
 
-        tar_feature = G(tar_input)
-        tar_feature_de = tar_feature.detach().cpu().numpy()
-        for count_tar in range(len(dset_loaders_tar) - 1):
-            tar_input = iter_tar.next()[0]
-            if use_gpu:
-                tar_input = Variable(tar_input).cuda()
-            else:
-                tar_input = Variable(tar_input)
-
-            tar_feature_new = G(tar_input)
-            tar_feature_new_de = tar_feature_new.detach().cpu().numpy()
-            tar_feature_de = np.append(tar_feature_de, tar_feature_new_de, axis=0)
-
-        # prepare validation feature and predicted label for validation
-        dsets_val = ImageList(val_cls_list[cls], transform=prep_dict)
-        dset_loaders_val = util_data.DataLoader(dsets_val, batch_size=batch_size, shuffle=True, num_workers=4)
-
+        count_val = 0
+        val_feature_de = np.array([])
         iter_val = iter(dset_loaders_val)
-        val_input, val_labels = iter_val.next()
-        if use_gpu:
-            val_input, val_labels = Variable(val_input).cuda(), Variable(val_labels).cuda()
-        else:
-            val_input, val_labels = Variable(val_input), Variable(val_labels)
-
-        val_feature = G(val_input)
-        pred_label = F1(val_feature)
-        val_feature_de = val_feature.detach().cpu().numpy()
-
-        w = pred_label[0].shape[0]
-        error = np.zeros(1)
-
-        error[0] = predict_loss(cls, pred_label[0].reshape(1, w)).item()
-        error = error.reshape(1, 1)
-        for num_image in range(1, len(pred_label)):
-            new_error = np.zeros(1)
-            single_pred_label = pred_label[num_image]
-            w = single_pred_label.shape[0]
-
-            new_error[0] = predict_loss(cls, single_pred_label.reshape(1, w)).item()
-            new_error = new_error.reshape(1, 1)
-            error = np.append(error, new_error, axis=0)
-        for count_val in range(len(dset_loaders_val) - 1):
+        while val_feature_de.size == 0:
             val_input, val_labels = iter_val.next()
-            if use_gpu:
-                val_input, val_labels = Variable(val_input).cuda(), Variable(val_labels).cuda()
-            else:
-                val_input, val_labels = Variable(val_input), Variable(val_labels)
-
-            val_feature_new = G(val_input)
-            val_feature_new_de = val_feature_new.detach().cpu().numpy()
-            val_feature_de = np.append(val_feature_de, val_feature_new_de, axis=0)
-            val_feature = G(val_input)
-            pred_label = F1(val_feature)
-
-            for num_image in range(len(pred_label)):
-                new_error = np.zeros(1)
-                single_pred_label = pred_label[num_image]
-                w = single_pred_label.shape[0]
-                new_error[0] = predict_loss(cls, single_pred_label.reshape(1, w)).item()
-                new_error = new_error.reshape(1, 1)
-            # cls should be a value, new_labels should be a [[x]] tensor format, the input format required by predict_loss
-                error = np.append(error, new_error, axis=0)
-            # error should be a (N, 1) numpy array, the input format required by get_dev_risk
-        print(src_feature_de.shape)
-        print(tar_feature_de.shape)
-        print(val_feature_de.shape)
-
-        # print(cls)
+            for i in range(len(val_labels)):
+                if val_labels[i].item() == cls:
+                    a, b, c = val_input[i].shape
+                    if use_gpu:
+                        val_pre_input, val_labels_final = Variable(val_input[i]).cuda(), Variable(val_labels[i]).cuda()
+                    else:
+                        val_pre_input, val_labels_final = Variable(val_input[i]), Variable(val_labels[i])
+                    val_input_final = val_pre_input.reshape(1, a, b, c)
+                    val_feature = G(val_input_final)
+                    pred_label = F1(val_feature)
+                    w = pred_label.shape[1]
+                    if val_feature_de.size == 0:
+                        # feature and error
+                        val_feature_de = val_feature.detach().cpu().numpy()
+                        error = np.zeros(1)
+                        error[0] = predict_loss(val_labels_final.item(), pred_label.reshape(1, w)).item()
+                        error = error.reshape(1, 1)
+                        print(error)
+                    else:
+                        # feature and error
+                        val_feature_new_de = val_feature.detach().cpu().numpy()
+                        val_feature_de = np.append(val_feature_de, val_feature_new_de, axis=0)
+                        new_error = np.zeros(1)
+                        new_error = new_error.reshape(1, 1)
+                        new_error[0] = predict_loss(val_labels_final.item(), pred_label.reshape(1, w)).item()
+                        error = np.append(error, new_error, axis=0)
+            count_val = count_val + 1
+        for _ in range(len(dset_loaders_val) - count_val):
+            val_input, val_labels = iter_val.next()
+            for i in range(len(val_labels)):
+                if val_labels[i].item() == cls:
+                    a, b, c = val_input[i].shape
+                    if use_gpu:
+                        val_pre_input, val_labels_final = Variable(val_input[i]).cuda(), Variable(val_labels[i]).cuda()
+                    else:
+                        val_pre_input, val_labels_final = Variable(val_input[i]), Variable(val_labels[i])
+                    val_input_final = val_pre_input.reshape(1, a, b, c)
+                    val_feature = G(val_input_final)
+                    pred_label = F1(val_feature)
+                    w = pred_label.shape[1]
+                    val_feature_new_de = val_feature.detach().cpu().numpy()
+                    val_feature_de = np.append(val_feature_de, val_feature_new_de, axis=0)
+                    new_error = np.zeros(1)
+                    new_error = new_error.reshape(1, 1)
+                    new_error[0] = predict_loss(val_labels_final.item(), pred_label.reshape(1, w)).item()
+                    error = np.append(error, new_error, axis=0)
+        print("Pass Validation for Class: {}".format(cls + 1))
+        print("Created error shape: {}".format(error.shape))
+        print("Created feature: {}".format(val_feature_de.shape))
+        # calculating the weight and the score for each class
         weight = get_weight(src_feature_de, tar_feature_de, val_feature_de)
-        cross_val_loss = cross_val_loss + get_dev_risk(weight, error)/class_num
-
+        cross_val_loss = cross_val_loss + get_dev_risk(weight, error) / class_num
 
     return cross_val_loss
--- visda_classification/res_train_main.py
+++ visda_classification/res_train_main.py
@@ -16,8 +16,30 @@ import source_risk
 import dev
 import dev_icml
 import seperate_data as sep
-
+import random
+import torch
+import math
+import sys
+import psutil
+import gc
+import os
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as util_data
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+import torch.utils.data as util_data
+from data_list import ImageList
+import pre_process as prep
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.autograd as autograd
+import seperate_data
+from basenet import *
 # Training settings
+
 parser = argparse.ArgumentParser(description='Visda Classification')
 parser.add_argument('--batch-size', type=int, default=13, metavar='N',
                     help='input batch size for training (default: 64)')
@@ -317,11 +339,166 @@ def train(num_epoch, option, num_layer, test_load, cuda):
                                                 224, batch_size,
                                                 use_gpu)
         else:
-            cv_loss = dev.cross_validation_loss(args, feature_network_path, predict_network_path, num_layer, cls_source_list,
-                                                target_path, cls_validation_list,
-                                                class_num, 256,
-                                                224, batch_size,
-                                                use_gpu)
+            cv_loss = dev.cross_validation_loss(args, feature_network_path, predict_network_path, num_layer,
+                                                     source_list,
+                                                     target_path, cls_validation_list,
+                                                     class_num, 256,
+                                                     224, batch_size,
+                                                     use_gpu)
+            # target_list_no_label = open(target_path).readlines()
+            # tar_cls_list = []
+            # cross_val_loss = 0
+            # save_path = args.save
+            # # load network
+            # print("Loaded network")
+            # # add pesudolabel for target data
+            # print("Sperating target data")
+            # target_list = []
+            # dsets_tar = ImageList(target_list_no_label,
+            #                       transform=prep.image_train(resize_size=256, crop_size=224))
+            # dset_loaders_tar = util_data.DataLoader(dsets_tar, batch_size=batch_size, shuffle=False, num_workers=4)
+            # len_train_target = len(dset_loaders_tar)
+            # iter_target = iter(dset_loaders_tar)
+            # count = 0
+            # for i in range(len_train_target):
+            #     input_tar, label_tar = iter_target.next()
+            #     if use_gpu:
+            #         input_tar, label_tar = Variable(input_tar).cuda(), Variable(label_tar).cuda()
+            #     else:
+            #         input_tar, label_tar = Variable(input_tar), Variable(label_tar)
+            #     tar_feature = G(input_tar)
+            #     predict_score = F1(tar_feature)
+            #     _, pre_lab = torch.max(predict_score, 1)
+            #     predict_label = pre_lab.detach()
+            #     for num in range(len(predict_label.cpu())):
+            #         if target_list_no_label[count][-3] == ' ':
+            #             ind = -2
+            #         else:
+            #             ind = -3
+            #         target_list.append(target_list_no_label[count][:ind])
+            #         target_list[count] = target_list[count] + str(predict_label[num].cpu().numpy()) + "\n"
+            #         count += 1
+            # # seperate the class
+            # for i in range(class_num):
+            #     tar_cls_list.append([j for j in target_list if int(j.split(" ")[1].replace("\n", "")) == i])
+            # prep_dict = prep.image_train(resize_size=256, crop_size=224)
+            # # load different class's image
+            # for cls in range(class_num):
+            #     print(cls)
+            #     print("Loading source data")
+            #     # prepare source feature
+            #     dsets_src = ImageList(cls_source_list[cls], transform=prep_dict)
+            #     dset_loaders_src = util_data.DataLoader(dsets_src, batch_size=batch_size, shuffle=True, num_workers=4)
+            #
+            #     iter_src = iter(dset_loaders_src)
+            #     src_input = iter_src.next()[0]
+            #     if use_gpu:
+            #         src_input = Variable(src_input).cuda()
+            #     else:
+            #         src_input = Variable(src_input)
+            #
+            #     src_feature = G(src_input)
+            #     src_feature_de = src_feature.detach().cpu().numpy()
+            #     for count_src in range(len(dset_loaders_src) - 1):
+            #         src_input = iter_src.next()[0]
+            #         if use_gpu:
+            #             src_input = Variable(src_input).cuda()
+            #         else:
+            #             src_input = Variable(src_input)
+            #
+            #         src_feature_new = G(src_input)
+            #         src_feature_new_de = src_feature_new.detach().cpu().numpy()
+            #         src_feature_de = np.append(src_feature_de, src_feature_new_de, axis=0)
+            #
+            #     np.save(os.path.join(save_path, 'src_feature_de.npy'), src_feature_de)
+            #     print("Loading target data")
+            #     # prepare target feature
+            #     dsets_tar = ImageList(tar_cls_list[cls], transform=prep_dict)
+            #     dset_loaders_tar = util_data.DataLoader(dsets_tar, batch_size=batch_size, shuffle=True, num_workers=4)
+            #
+            #     iter_tar = iter(dset_loaders_tar)
+            #     tar_input = iter_tar.next()[0]
+            #     if use_gpu:
+            #         tar_input = Variable(tar_input).cuda()
+            #     else:
+            #         tar_input = Variable(tar_input)
+            #
+            #     tar_feature = G(tar_input)
+            #     tar_feature_de = tar_feature.detach().cpu().numpy()
+            #     for count_tar in range(len(dset_loaders_tar) - 1):
+            #         tar_input = iter_tar.next()[0]
+            #         if use_gpu:
+            #             tar_input = Variable(tar_input).cuda()
+            #         else:
+            #             tar_input = Variable(tar_input)
+            #
+            #         tar_feature_new = G(tar_input)
+            #         tar_feature_new_de = tar_feature_new.detach().cpu().numpy()
+            #         tar_feature_de = np.append(tar_feature_de, tar_feature_new_de, axis=0)
+            #
+            #     np.save(os.path.join(save_path, 'tar_feature_de.npy'), tar_feature_de)
+            #     print("Loading validation data")
+            #     # prepare validation feature and predicted label for validation
+            #     dsets_val = ImageList(cls_validation_list[cls], transform=prep_dict)
+            #     dset_loaders_val = util_data.DataLoader(dsets_val, batch_size=batch_size, shuffle=True, num_workers=4)
+            #
+            #     iter_val = iter(dset_loaders_val)
+            #     val_input, val_labels = iter_val.next()
+            #     if use_gpu:
+            #         val_input, val_labels = Variable(val_input).cuda(), Variable(val_labels).cuda()
+            #     else:
+            #         val_input, val_labels = Variable(val_input), Variable(val_labels)
+            #
+            #     val_feature = G(val_input)
+            #     pred_label = F1(val_feature)
+            #     val_feature_de = val_feature.detach().cpu().numpy()
+            #
+            #     w = pred_label[0].shape[0]
+            #     error = np.zeros(1)
+            #
+            #     error[0] = dev.predict_loss(cls, pred_label[0].reshape(1, w)).item()
+            #     error = error.reshape(1, 1)
+            #     for num_image in range(1, len(pred_label)):
+            #         new_error = np.zeros(1)
+            #         single_pred_label = pred_label[num_image]
+            #         w = single_pred_label.shape[0]
+            #
+            #         new_error[0] = dev.predict_loss(cls, single_pred_label.reshape(1, w)).item()
+            #         new_error = new_error.reshape(1, 1)
+            #         error = np.append(error, new_error, axis=0)
+            #     for count_val in range(len(dset_loaders_val) - 1):
+            #         val_input, val_labels = iter_val.next()
+            #         if use_gpu:
+            #             val_input, val_labels = Variable(val_input).cuda(), Variable(val_labels).cuda()
+            #         else:
+            #             val_input, val_labels = Variable(val_input), Variable(val_labels)
+            #
+            #         val_feature_new = G(val_input)
+            #         val_feature_new_de = val_feature_new.detach().cpu().numpy()
+            #         val_feature_de = np.append(val_feature_de, val_feature_new_de, axis=0)
+            #         val_feature = G(val_input)
+            #         pred_label = F1(val_feature)
+            #
+            #         for num_image in range(len(pred_label)):
+            #             new_error = np.zeros(1)
+            #             single_pred_label = pred_label[num_image]
+            #             w = single_pred_label.shape[0]
+            #             new_error[0] = dev.predict_loss(cls, single_pred_label.reshape(1, w)).item()
+            #             new_error = new_error.reshape(1, 1)
+            #             # cls should be a value, new_labels should be a [[x]] tensor format, the input format required by predict_loss
+            #             error = np.append(error, new_error, axis=0)
+            #         # error should be a (N, 1) numpy array, the input format required by get_dev_risk
+            #     np.save(os.path.join(save_path, 'val_feature_de.npy'), val_feature_de)
+            #     print("Finish printing")
+            #     # print(src_feature_de.shape)
+            #     # print(tar_feature_de.shape)
+            #     # print(val_feature_de.shape)
+            #
+            #     # print(cls)
+            #     # weight = dev.get_weight(os.path.join(save_path, 'src_feature_de.npy'),
+            #     #                     os.path.join(save_path, 'tar_feature_de.npy'),
+            #     #                     os.path.join(save_path, 'val_feature_de.npy'))
+            #     # cv_loss = cross_val_loss + dev.get_dev_risk(weight, error) / class_num
         print(cv_loss)
 
 def test(epoch):
